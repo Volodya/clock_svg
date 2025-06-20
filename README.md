e2a901798e5a769e0b6a8ec91a7da30263e52b84
Volodya <VladimirMozhenkov@yahoo.com>
2025-06-20 11:23:00 +0300

    Additing the ability to change colours

--- README.md
+++ README.md
@@ -20,3 +20,23 @@ To run:
 To save into a file:
 
     ./clock_svg 7 13 > clock_7_13.svg
+
+To change the colour:
+
+    ./clock_svg --colour=pink 7 13
+  
+  or to change only face colour
+  
+    ./clock_svg --facecolour=yellow 7 13
+  
+  or to change only colour of hands
+  
+    ./clock_svg --handcolour=red 7 13
+
+  or to change colour of hands to a different colour each
+  
+    ./clock_svg --minutehandcolour=yellow --hourhandcolour=red 7 13
+
+Last colour applied will take presedence, all colours acceptable by the SVG fill attribute are acceptable.
+
+  see also: https://developer.mozilla.org/en-US/docs/Web/SVG/Reference/Attribute/fill
--- main.cpp
+++ main.cpp
@@ -5,13 +5,41 @@
 int main(int argc, char* argv[])
 {
 	int hour{0}, minute{0};
-	if(argc>1)
+	std::string faceColour{"black"}, hourHandColour{"black"}, minuteHandColour{"black"};
+	
+	int gnuArgCount = 0;
+	while(argv[gnuArgCount+1] && argv[gnuArgCount+1][0] == '-' && argv[gnuArgCount+1][1] == '-')
 	{
-		hour = std::stoi(argv[1]);
+		std::string gnuArg{&argv[gnuArgCount+1][2]};
+		if(gnuArg.starts_with("colour="))
+		{
+			faceColour = hourHandColour = minuteHandColour = gnuArg.substr(std::string{"colour="}.length());
+		}
+		else if(gnuArg.starts_with("facecolour="))
+		{
+			faceColour = gnuArg.substr(std::string{"facecolour="}.length());
+		}
+		else if(gnuArg.starts_with("handcolour="))
+		{
+			hourHandColour = minuteHandColour = gnuArg.substr(std::string{"handcolour="}.length());
+		}
+		else if(gnuArg.starts_with("hourhandcolour="))
+		{
+			hourHandColour = gnuArg.substr(std::string{"hourhandcolour="}.length());
+		}
+		else if(gnuArg.starts_with("minutehandcolour="))
+		{
+			minuteHandColour = gnuArg.substr(std::string{"minutehandcolour="}.length());
+		}
+		++gnuArgCount;
 	}
-	if(argc>2)
+	if(argc>gnuArgCount+1)
 	{
-		minute = std::stoi(argv[2]);
+		hour = std::stoi(argv[gnuArgCount + 1]);
+	}
+	if(argc>gnuArgCount+2)
+	{
+		minute = std::stoi(argv[gnuArgCount + 2]);
 	}
 	
 	hour %= 12;
@@ -22,8 +50,8 @@ int main(int argc, char* argv[])
 		<< "<?xml version='1.0' encoding='UTF-8'?>" << std::endl
 		<< "<svg xmlns:xlink='http://www.w3.org/1999/xlink' xmlns:svg='http://www.w3.org/2000/svg' xmlns='http://www.w3.org/2000/svg' width='500' height='500' viewBox='-100 -100 200 200'>" << std::endl
 		<< "<defs>"
-			<< "<circle cx='0' cy='87' r='2.2' fill='black' id='minMarker'/>"
-			<< "<line x1='0' y1='95' x2='0' y2='78' stroke-width='3.8' stroke='black' id='hourMarker'/>"
+			<< "<circle cx='0' cy='87' r='2.2' fill='" << faceColour << "' id='minMarker'/>"
+			<< "<line x1='0' y1='95' x2='0' y2='78' stroke-width='3.8' stroke='" << faceColour << "' id='hourMarker'/>"
 		<< "</defs>" << std::endl
 		<< "<g id='markerSet'>"
 			<< "<use xlink:href='#hourMarker'/>"
@@ -36,10 +64,10 @@ int main(int argc, char* argv[])
 	{
 		std::cout << "<use xlink:href='#markerSet' transform='rotate(" << degree << ")'/>" << std::endl;
 	}
-	std::cout << "<circle cx='0' cy='0' r='9' fill='black'/>" << std::endl;
+	std::cout << "<circle cx='0' cy='0' r='9' fill='" << faceColour << "'/>" << std::endl;
 	
-	std::cout << "<line x1='0' y1='-62' x2='0' y2='0' stroke-width='5' stroke='black' transform='rotate(" << (hour + static_cast<float>(minute)/60) * (360/12) << ")'/>" << std::endl;
-	std::cout << "<line x1='0' y1='-95' x2='0' y2='0' stroke-width='2.8' stroke='black' transform='rotate(" << minute * (360/60) << ")'/>" << std::endl;
+	std::cout << "<line x1='0' y1='-62' x2='0' y2='0' stroke-width='5' stroke='" << hourHandColour << "' transform='rotate(" << (hour + static_cast<float>(minute)/60) * (360/12) << ")'/>" << std::endl;
+	std::cout << "<line x1='0' y1='-95' x2='0' y2='0' stroke-width='2.8' stroke='" << minuteHandColour << "' transform='rotate(" << minute * (360/60) << ")'/>" << std::endl;
 	
 	std::cout << "</svg>" << std::endl;
 }
